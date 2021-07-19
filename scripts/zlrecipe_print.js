1608c58bfb52f1c939f633d12f6c039a021b5bf3
Rogier Lankhorst <rogier@really-simple-plugins.com>
2021-07-19 09:07:58 +0200

    print output with filename

--- scripts/zlrecipe_print.js
+++ scripts/zlrecipe_print.js
@@ -2,10 +2,14 @@ var win=null;
 function zlrPrint(id, plugin_path)
 {
 	var content = document.getElementById(id).innerHTML;
+	var title = document.getElementsByClassName("zrdn-element_recipe_title")[0].innerHTML;
 	win = window.open();
 	self.focus();
 	win.document.open();
-	win.document.write('<html><head>');
+	win.document.write('<html><title>');
+	win.document.write(title);
+	win.document.write('</title>');
+	win.document.write('<head>');
 	win.document.write('<link charset="utf-8" href="'+zrdn_print_styles.grid_style+'" rel="stylesheet" type="text/css" />');
 	win.document.write('<link charset="utf-8" href="'+zrdn_print_styles.stylesheet_url+'" rel="stylesheet" type="text/css" />');
 	win.document.write('<link charset="utf-8" href="'+zrdn_print_styles.print_css+'" rel="stylesheet" type="text/css" />');
